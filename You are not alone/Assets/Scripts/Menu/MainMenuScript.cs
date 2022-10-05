8e914155612f65991da91392b155dd6260cad79d
Michael2150 <michaelgerb2150@gmail.com>
2022-10-05 19:14:45 +0100

    Removed extra assets. finished menu and gamemode screen

--- You are not alone/Assets/Scripts/Menu/MainMenuScript.cs
+++ You are not alone/Assets/Scripts/Menu/MainMenuScript.cs
@@ -3,18 +3,49 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class MainMenuScript : MonoBehaviour
 {
     [SerializeField] private Button _playButton;
     [SerializeField] private Button _modeButton;
     [SerializeField] private Button _exitButton;
+    [SerializeField] private TMP_InputField _edtSeed;
+    [SerializeField] private Button _btnLastSeed;
+    [SerializeField] private TMP_Text _txtLastSeed;
+    
+    [SerializeField] private long _seed;
+    [SerializeField] private long _lastSeed;
+    
+    public long lastSeed
+    {
+        get => _lastSeed;
+        set
+        {
+            _lastSeed = value;
+            _txtLastSeed.text = "Last Seed: " + _lastSeed.ToString();
+        }
+    }
+    public long seed
+    {
+        get =>  long.Parse(_edtSeed.text);
+        set
+        {
+            _seed = value;
+            _edtSeed.text = _seed.ToString();
+        }
+    }
 
     private void Start()
     {
         _playButton.onClick.AddListener(Play);
         _modeButton.onClick.AddListener(Mode);
         _exitButton.onClick.AddListener(Exit);
+        _btnLastSeed.onClick.AddListener(copySeed);
+        
+        long randomSeed = (long)Random.Range(1000000000000000, 9999999999999999);
+        seed = randomSeed;
+        lastSeed = randomSeed;
     }
     
     private void Play()
@@ -25,6 +56,11 @@ public class MainMenuScript : MonoBehaviour
     private void Mode()
     {
         Debug.Log("Mode");
+    }
+    
+    private void copySeed()
+    {
+        GUIUtility.systemCopyBuffer = lastSeed.ToString();
     }
     
     private void Exit()
